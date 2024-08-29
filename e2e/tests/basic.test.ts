7067f15ac50803e7dc9e4a00f6843930ce2c8d41
LoicBrassart <loic.brassart@gmail.com>
2024-08-29 19:07:26 +0200

    fix: make CI work at last!

--- e2e/tests/basic.test.ts
+++ e2e/tests/basic.test.ts
@@ -1,4 +1,6 @@
 import { expect, test } from "@playwright/test";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 test("Go to home page", async ({ page }) => {
   await page.goto(process.env.WEBSITE_URL);
