a6a2ecf0677532cbace25cef6a236714801d8c13
LoicBrassart <LoicBrassart@users.noreply.github.com>
2024-08-30 10:08:10 +0200

    Dev (#16)

--- e2e/tests/basic.test.ts
+++ e2e/tests/basic.test.ts
@@ -3,6 +3,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 test("Go to home page", async ({ page }) => {
+  if (!process.env.WEBSITE_URL) return;
   await page.goto(process.env.WEBSITE_URL);
 
   await page.waitForLoadState("networkidle");
